4925b5dbdfebf5aeff64a38b9eafcc1ced951d7a
Sandeep Pal <skumar8work@gmail.com>
2023-08-17 09:44:16 +0530

    Update 009_Palindrome_Number.py

--- 009_Palindrome_Number.py
+++ 009_Palindrome_Number.py
@@ -1,18 +1,4 @@
 class Solution:
-    # @return a boolean
-    def isPalindrome(self, x):
-        if x < 0:
-            return False
-        copy, reverse = x, 0
-        
-        while copy:
-            reverse *= 10
-            reverse += copy % 10
-            copy /= 10
-        
-        return x == reverse
-
-if __name__ == "__main__":
-    print Solution().isPalindrome(98789)
-    print Solution().isPalindrome(36726)
-    print Solution().isPalindrome(-45654)
+    def isPalindrome(self, x: int) -> bool:
+        temp = x
+        return True if str(temp) == str(x)[::-1] else False
