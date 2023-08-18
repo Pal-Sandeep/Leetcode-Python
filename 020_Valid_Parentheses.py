3de525e54ea238acdc57ba8b4f16248ceecb0726
Sandeep Pal <skumar8work@gmail.com>
2023-08-18 08:45:42 +0530

    Create 020_Valid_Parentheses.py

---
+++ 020_Valid_Parentheses.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while s:
+            if '{}' in s: s = s.replace('{}', '')
+            elif '[]' in s: s = s.replace('[]', '')
+            elif '()' in s: s = s.replace('()', '')
+            else: return False
+        return True
